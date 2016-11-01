aa70bac5d125af2501e2b962622183d5ddd31954
STEPHANIE SUTANTO <stph.sutanto@gmail.com>
2016-11-01 20:45:48 +0800

    invites

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -1,2 +1,12 @@
 class InvitesController < ApplicationController
+
+	def create
+		@invite = Invite.new(invite_params)
+		@invite.sender_id = current_user.id
+		if @invite.save
+			InviteMailer.new_user_invite(@invite, new_user_registration_url(:invite_token => @invite.token)).deliver
+		else
+			redirect_to :back, notice: "Unsuccessful"
+		end
+	end
 end
--- app/models/invite.rb
+++ app/models/invite.rb
@@ -3,4 +3,8 @@ class Invite < ActiveRecord::Base
 	belongs_to :group
 	belongs_to :sender, :class_name => 'User'
 	belongs_to :recipient, :class_name => 'User'
+
+	def generate_token
+		self.token = Digest::SHA1.hexdigest([self.group_id, Time.now, rand].join)
+	end
 end
